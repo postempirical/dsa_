e21f3588f3d15ba6ca81c9cf9946a436d53c94c6
postempirical <53400689+postempirical@users.noreply.github.com>
2021-07-01 21:20:05 +0530

    Update twoSum.java

--- arrays/twoSum.java
+++ arrays/twoSum.java
@@ -1,6 +1,6 @@
 // Print an array of two values that equal target sum 
 
-// [1] -> Time: O (n^2), Space: O(1)  
+// [1] -> Time: O(n^2), Space: O(1)  
 class twoSum {
   public static int[] twoNumberSum(int[] array, int targetSum) {
 		for (int i = 0; i < array.length - 1; i++) {
