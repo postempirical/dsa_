f6b11adb20cb28d2126617456bbae2475f12a644
postempirical <53400689+postempirical@users.noreply.github.com>
2021-07-26 19:15:32 +0530

    Update reverseLL.java

--- linkedList/reverseLL.java
+++ linkedList/reverseLL.java
@@ -16,6 +16,20 @@ class Program {
 		head = mid;
 		return mid;
   }
+	
+// [2] -> Time: O(n), Space: O(1)  
+	public static LinkedList reverseLinkedList(LinkedList head) {
+	// sliding(2) -- previous >> current
+    LinkedList current = head;
+		LinkedList previous = null; 
+    while (current != null) {
+			LinkedList nextNode = current.next;
+			current.next = previous;
+			previous = current;
+			current = nextNode;
+		}
+		return previous;
+  }
 
   static class LinkedList {
     int value;
