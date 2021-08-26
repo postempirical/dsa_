4ad56b4d9acc959f82e65f5bdad36e602cf603ca
postempirical <53400689+postempirical@users.noreply.github.com>
2021-08-26 14:58:02 +0530

    Create dfs.java

---
+++ striver/graphs/dfs.java
@@ -0,0 +1,24 @@
+class Solution
+{
+    public void dfs(int node, boolean vis[], ArrayList<ArrayList<Integer>> adj, ArrayList<Integer> storeDfs) {
+        storeDfs.add(node); 
+        vis[node] = true; 
+        for(Integer it: adj.get(node)) {
+            if(vis[it] == false) {
+                dfs(it, vis, adj, storeDfs); 
+            }
+        }
+    }
+  
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj)
+    {
+        ArrayList<Integer> storeDfs = new ArrayList<>(); 
+        boolean vis[] = new boolean[V+1]; 
+        for(int i = 1;i<=V;i++) {
+            if(!vis[i]) dfs(i, vis, adj, storeDfs); 
+        }
+        
+        return storeDfs;
+    }
+}
+
