a0ea37791494c8f969f34c919f7b257a1c7c3e4b
postempirical <53400689+postempirical@users.noreply.github.com>
2022-02-06 09:23:29 +0000

    Create bst_validate.java

---
+++ striver/trees/bst_validate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    
+    public boolean helper(TreeNode root, long min, long max) {
+        if (root == null) return true;
+        if (root.val >= max || root.val <= min) return false;
+        return helper(root.left, min, root.val) 
+            && helper(root.right, root.val, max);
+    }
+}
