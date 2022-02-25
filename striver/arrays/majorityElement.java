bc8a02f56e7add7649384694d98f914297e607f7
postempirical <53400689+postempirical@users.noreply.github.com>
2022-02-25 12:20:00 +0000

    Update majorityElement.java

--- striver/arrays/majorityElement.java
+++ striver/arrays/majorityElement.java
@@ -1,3 +1,4 @@
+// MOJORITY N / 2
 // Sorting
 public int majorityElement1(int[] nums) {
     Arrays.sort(nums);
@@ -33,6 +34,51 @@ class Solution {
             else if (num == res) count++;
             else count--;
         }
+        return res;
+    }
+}
+
+
+// MOJORITY N / 3
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        int num1 = -1; int num2 = -1; 
+        int count1 = 0; int count2 = 0;
+        
+        for (int val : nums) {
+            if(val == num1) 
+                count1++;
+            else if (val == num2) 
+                count2++;
+            
+            else if (count1 == 0) {
+                num1 = val;
+                count1++;
+                }
+            else if (count2 == 0) {
+                num2 = val;
+                count2++;
+            }
+            else {
+                count1--;
+                count2--;
+            }
+        }
+        
+        count1 = 0;
+        count2 = 0;
+        for(int val : nums) {
+            if(val == num1) 
+                count1++;
+            else if(val == num2) 
+                count2++;
+        }
+        
+        if(count1 > nums.length/3)
+            res.add(num1);
+        if(count2 > nums.length/3)
+            res.add(num2);
         return res;
     }
 }
