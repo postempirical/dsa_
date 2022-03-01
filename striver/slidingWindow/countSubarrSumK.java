784b1d2b89a148000f17c863f5599da4701b7dfa
postempirical <53400689+postempirical@users.noreply.github.com>
2022-03-01 17:25:28 +0000

    Update countSubarrSumK.java

--- striver/slidingWindow/countSubarrSumK.java
+++ striver/slidingWindow/countSubarrSumK.java
@@ -22,3 +22,40 @@ class Solution {
         return ans;
     }
 }
+
+
+// longest subarr of sum k
+// here instead of count we need max length of subarr
+
+
+static int lenOfLongSubarr(int[] arr, int n, int k)
+{
+			 // HashMap to store (sum, index) tuples
+			 HashMap<Integer, Integer> map = new HashMap<>();
+			 int sum = 0, maxLen = 0;
+
+			 // traverse the given array
+			 for (int i = 0; i < n; i++) {
+						// accumulate sum
+						sum += arr[i];
+
+						// when subarray starts from index '0'
+						if (sum == k)
+								maxLen = i + 1;
+
+						// make an entry for 'sum' if it is
+						// not present in 'map'
+						if (!map.containsKey(sum)) {
+								map.put(sum, i);
+						}
+
+						// check if 'sum-k' is present in 'map'
+						// or not
+						if (map.containsKey(sum - k)) {
+								// update maxLength
+								if (maxLen < (i - map.get(sum - k)))
+										maxLen = i - map.get(sum - k);
+						}
+			 }
+			 return maxLen;            
+}
