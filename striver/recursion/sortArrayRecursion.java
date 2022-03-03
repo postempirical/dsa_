2b7282d27ea677c0ebe89c7ec9c1eb1abf97a682
postempirical <53400689+postempirical@users.noreply.github.com>
2022-03-03 11:21:06 +0000

    Create sortArrayRecursion.java

---
+++ striver/recursion/sortArrayRecursion.java
@@ -0,0 +1,39 @@
+// sort an array using recursion
+public class SortAnArray {
+	private static List<Integer> sort(List<Integer> l) {
+		if(l.size() == 1)
+			return l;
+		//hypothesis
+		Integer temp = l.get(l.size()-1);
+		l.remove(l.size()-1);
+		l = sort(l);
+		//induction
+		return l = insert(l, temp);
+	}
+
+	private static List<Integer> insert(List<Integer> l, int i) {
+		if(l.size() == 0 || i >= l.get(l.size()-1))
+		{
+			l.add(i);
+			return l;
+		}
+		//hypothesis
+		int val = l.get(l.size() - 1);
+		l.remove(l.size()-1);
+		l = insert(l, i);
+		//induction
+		l.add(val);
+		return l;
+	}
+  
+  public static void main(String[] args) {
+		List<Integer> l = new ArrayList();
+		l.add(1);
+		l.add(0);
+		l.add(5);
+		l.add(2);
+		l = sort(l);
+		for(int i : l)
+			System.out.println(i);
+	}
+}
