86cbc9358aefc4fe0f4925cccaa2eec8653114d9
postempirical <53400689+postempirical@users.noreply.github.com>
2022-03-03 11:30:25 +0000

    Update sortArrayRecursion.java

--- striver/recursion/sortArrayRecursion.java
+++ striver/recursion/sortArrayRecursion.java
@@ -3,30 +3,35 @@ public class SortAnArray {
 	private static List<Integer> sort(List<Integer> list) {
 		if(list.size() == 1) return l;
 		
-		//hypothesis
+		// hypothesis
+		// pop and store last value of list
+		// call sort() on decreased input
 		Integer temp = list.get(list.size() - 1);
 		list.remove(list.size() - 1);
 		list = sort(list);
 		
-		//induction
+		// induction
+		// call insert() for putting popped value into correct pos
 		insert(list, temp);
 		
 		return list;
 	}
 
 	private static List<Integer> insert(List<Integer> l, int elem) {
-		if(list.size() == 0 || elem >= list.get(list.size() - 1))
-		{
+		// if inserted elem is largest just insert it in the end
+		if(list.size() == 0 || elem >= list.get(list.size() - 1)) {
 			l.add(elem);
 			return list;
 		}
 		
-		//hypothesis
+		// hypothesis
+		// pop last val and store it, call insert on new input
 		int val = list.get(list.size() - 1);
 		list.remove(list.size() - 1);
 		list = insert(list, elem);
 		
-		//induction
+		// induction
+		// add popped value back
 		list.add(val);
 		return list;
 	}
