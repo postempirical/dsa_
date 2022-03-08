5db453b1292c712124ae4a2661e1bb2962b32327
postempirical <53400689+postempirical@users.noreply.github.com>
2022-03-08 05:55:17 +0000

    Update combSum2.java

--- striver/recursion/combSum2.java
+++ striver/recursion/combSum2.java
@@ -1,27 +1,35 @@
 // tc = 2^n * k
 class Solution {
-    public void findCombinations(int ind, int[] arr, int target, 
-                               List<List<Integer>> ans,
-                               ArrayList<Integer> ds) {
-        if (target == 0) {
-            ans.add(new ArrayList<>(ds));
-            return;
-        }
-        for (int i = ind; i < arr.length; i++) {
-            if (i > ind && arr[i] == arr[i - 1]) continue;
-            if (arr[i] > target) break;
-        
-            ds.add(arr[i]);
-            findCombinations(i + 1, arr, target - arr[i], ans, ds);
-            ds.remove(ds.size() - 1);
-        }
+    public List<List<Integer>> combinationSum2(int[] arr, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        Arrays.sort(arr);
+        int ind = 0;
+        solve(ind, target, list, arr, ans);
+        return ans;
     }
     
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        ArrayList<Integer> ds = new ArrayList<>();
-        Arrays.sort(candidates);
-        findCombinations(0, candidates, target, ans, ds);
-        return ans;
+    public void solve(int ind, int target, List<Integer> list, int[] arr, List<List<Integer>> ans) {
+        // base cond
+        if (target == 0) {
+            ans.add(new ArrayList<>(list));
+            return;
+        }
+        
+        // looping for each ind
+        for (int i = ind; i < arr.length; i++) {
+            // if its same as prev val, skip
+            // if i is first val then dont skip
+            // [1,1,2,4,5] we will miss 114 for t=6
+            // so amke sure i is more than index
+            if (i > ind && arr[i] == arr[i - 1]) continue;
+            
+            // sorted array so break asap
+            if (arr[i] > target) break;
+            
+            list.add(arr[i]);
+            solve(i + 1, target - arr[i], list, arr, ans);
+            list.remove(list.size() - 1);
+        }
     }
 }
