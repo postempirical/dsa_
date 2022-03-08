a8f4c71cb6b8faaf4491361cff15e0cc11f5ae48
postempirical <53400689+postempirical@users.noreply.github.com>
2022-03-08 07:22:27 +0000

    Update subsetSum1.java

--- striver/recursion/subsetSum1.java
+++ striver/recursion/subsetSum1.java
@@ -18,3 +18,21 @@ class Solution {
         }
     }
 }
+
+
+// pick not pick approach for ss1
+public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(result, new ArrayList<Integer>(), nums, 0);
+        return result;
+    }
+    void backtrack(List<List<Integer>> result, List<Integer> tempList, int[] nums, int start){
+        if(nums.length == start){
+            result.add(new ArrayList<>(tempList));    
+            return;
+        }
+        tempList.add(nums[start]);
+        backtrack(result, tempList, nums, start+1);
+        tempList.remove(tempList.size()-1);
+        backtrack(result, tempList, nums, start+1);
+    }
