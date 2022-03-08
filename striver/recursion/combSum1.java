5460bdd430aba3657c932fb3fa15a89853b52188
postempirical <53400689+postempirical@users.noreply.github.com>
2022-03-08 05:21:29 +0000

    Update combSum1.java

--- striver/recursion/combSum1.java
+++ striver/recursion/combSum1.java
@@ -1,28 +1,28 @@
-// tc = 2^e * k
+// tc = 2^t * k
 class Solution {
-    public void findCombinations(int ind, int[] arr, int target, 
-                                 List<List<Integer>> ans, 
-                                 ArrayList<Integer> ds) {
-        if (ind == arr.length) {
-            if (target == 0) {
-                ans.add(new ArrayList<>(ds));
-            }
-            return;
-        }
-        
-        if (arr[ind] <= target) {
-            ds.add(arr[ind]);
-            findCombinations(ind, arr, target - arr[ind], ans, ds);
-            ds.remove(ds.size() - 1);
-        }
-        findCombinations(ind + 1, arr, target, ans, ds);
-    }
-  
-      public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    public List<List<Integer>> combinationSum(int[] arr, int target) {
         List<List<Integer>> ans = new ArrayList<>();
-        ArrayList<Integer> ds = new ArrayList<>();
-        findCombinations(0, candidates, target, ans, ds); 
-        return ans; 
+        List<Integer> list = new ArrayList<>();
+        int ind = 0;
+        solve(ind, target, list, arr, ans);
+        return ans;
     }
     
+    public void solve(int ind, int target, List<Integer> list, int[] arr, List<List<Integer>> ans) {
+        // if viewed all indices and targets gets 0
+        if (ind == arr.length) {
+            if (target == 0) ans.add(new ArrayList<>(list));
+        return;
+        }
+        
+        // keep dec target til val is smaller
+        // call on same ind with decreased target
+        // else momve to next ind after backtracking 
+        if (arr[ind] <= target) {
+            list.add(arr[ind]);
+            solve(ind, target - arr[ind], list, arr, ans);
+            list.remove(list.size() - 1);
+        }
+        solve(ind + 1, target, list, arr, ans);    
+    }
 }
