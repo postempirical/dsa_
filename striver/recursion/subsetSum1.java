7703a700e25dd707e4001ec448634f92a0332ae0
postempirical <53400689+postempirical@users.noreply.github.com>
2022-03-08 07:17:54 +0000

    Update subsetSum1.java

--- striver/recursion/subsetSum1.java
+++ striver/recursion/subsetSum1.java
@@ -10,6 +10,8 @@ class Solution {
     public void solve(int ind, List<Integer> list, int[] nums, List<List<Integer>> ans) {
         ans.add(new ArrayList<>(list));
         for (int i = ind; i < nums.length; i++) {
+						// subset2 no duplicates
+					  // if(i > ind && nums[i] == nums[i - 1]) continue;
             list.add(nums[i]);
             solve(i + 1, list, nums, ans);
             list.remove(list.size() - 1);
