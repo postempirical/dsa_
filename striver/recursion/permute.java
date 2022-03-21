61922597bc4d064f5de32d6102a32332e6e825cb
postempirical <53400689+postempirical@users.noreply.github.com>
2022-03-21 07:59:39 +0000

    Update permute.java

--- striver/recursion/permute.java
+++ striver/recursion/permute.java
@@ -1,4 +1,58 @@
 // https://leetcode.com/problems/permutations
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        solve(ans, list, nums);
+        return ans;
+    }
+    
+    public void solve(List<List<Integer>> ans, List<Integer> list, int[] nums) {
+        if (list.size() == nums.length) 
+            ans.add(new ArrayList<>(list));
+        else {
+            for (int i = 0; i < nums.length; i++) {
+                if (list.contains(nums[i])) continue;
+                list.add(nums[i]);
+                solve(ans, list, nums);
+                list.remove(list.size() - 1);
+            }
+        }
+    }
+}
+
+
+// https://leetcode.com/problems/permutations-ii/
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+        Arrays.sort(nums);
+        solve(ans, list, nums, used);
+        return ans;
+    }
+    public void solve(List<List<Integer>> ans, 
+                      List<Integer> list, int[] nums, boolean[] used) {
+        if (list.size() == nums.length) 
+            ans.add(new ArrayList<>(list));
+        else {
+            for (int i = 0; i < nums.length; i++) {
+								// if already used skip this
+                if (used[i]) continue; 
+								// if equal to prev and prev is not used skip
+								// as prev will be marked used later on
+                if (i > 0 && nums[i] == nums[i - 1] && 
+                    !used[i - 1]) continue;
+                used[i] = true;
+                list.add(nums[i]);
+                solve(ans, list, nums, used);
+                used[i] = false;
+                list.remove(list.size() - 1);
+            }
+        }
+    }
+}
 // tc = n! * n, sc = n + n
 class Solution {
     public void recur(int[] nums, List<List<Integer>> ans, 
