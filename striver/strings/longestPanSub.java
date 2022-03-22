325509668bc29dc79e1270d5b9fb63a227a818a8
postempirical <53400689+postempirical@users.noreply.github.com>
2022-03-22 06:24:39 +0000

    Update longestPanSub.java

--- striver/strings/longestPanSub.java
+++ striver/strings/longestPanSub.java
@@ -3,20 +3,26 @@ class Solution {
     public String longestPalindrome(String s) {
         int start = 0, end = 0;
         for (int i = 0; i < s.length(); i++) {
+						// place both at the cureent char
             int left = i, right = i;
             char c = s.charAt(i);
             
+						// updation for even char 
             while (left >= 0 && s.charAt(left) == c) left--;
             while (right < s.length() && s.charAt(right) == c) right++;
             
+						// check palindrome in range of current l,r
             while (left >= 0 && right < s.length()) {
                 if (s.charAt(left) != s.charAt(right)) break;
                 left--;
                 right++;
             }
+					
+						// get them in actual range
             left = left + 1;
             right = right - 1;
             
+						// update max
             if (end - start < right - left) {
                 start = left;
                 end = right;
