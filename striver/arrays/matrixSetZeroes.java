47ee6d6480fb3dc9f8018c28fe6305955db99d1f
postempirical <53400689+postempirical@users.noreply.github.com>
2022-03-28 09:50:31 +0000

    Update matrixSetZeroes.java

--- striver/arrays/matrixSetZeroes.java
+++ striver/arrays/matrixSetZeroes.java
@@ -1,23 +1,28 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        boolean isColZero = false;
+        boolean colzero = false;
         int row = matrix.length;
         int col = matrix[0].length;
         
         for (int i = 0; i < row; i++) {
-            if (matrix[i][0] == 0) isColZero = true;
+            // flip colzero if 0th col has a 0
+            if (matrix[i][0] == 0) colzero = true;
             for (int j = 1; j < col; j++) {
+                // update starting row,col
                 if (matrix[i][j] == 0) 
                     matrix[i][0] = matrix[0][j] = 0;
             }
         }
         
+        // traverse backwards to eliminate col0 issue
         for (int i = row - 1; i >= 0; i--) {
             for (int j = col - 1; j >= 1; j--) {
+                // if start r,c has 0, insert 0 in cell
                 if (matrix[i][0] == 0 || matrix[0][j] == 0) 
                     matrix[i][j] = 0;
             }
-            if (isColZero == true) matrix[i][0] = 0;
+            // insert 0 in col[0] if true
+            if (colzero == true) matrix[i][0] = 0;
         }
     }
 }
