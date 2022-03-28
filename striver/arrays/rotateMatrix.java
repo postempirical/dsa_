a24098582e4a91b598143682b69cffe80709c9de
postempirical <53400689+postempirical@users.noreply.github.com>
2022-03-28 11:11:02 +0000

    Update rotateMatrix.java

--- striver/arrays/rotateMatrix.java
+++ striver/arrays/rotateMatrix.java
@@ -21,3 +21,27 @@ class Solution {
         }
     }
 }
+
+// swap function
+class Solution {
+    public void rotate(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix[0].length; j++) {
+                swap(matrix, i, j, j, i);
+            }
+        }
+        
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length / 2; j++) {
+                swap(matrix, i, j, i, matrix.length - j - 1);
+            }
+        }
+    }
+    
+    public void swap(int[][] matrix, int i, int j, int s, int t) {
+        int temp = 0;
+        temp = matrix[i][j];
+        matrix[i][j] = matrix[s][t];
+        matrix[s][t] = temp;
+    }
+}
