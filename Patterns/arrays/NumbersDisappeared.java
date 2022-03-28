8d96d81d620fa9fd3b4ae9e6ddccf381aaf4f97d
postempirical <53400689+postempirical@users.noreply.github.com>
2022-03-28 06:37:05 +0000

    Update NumbersDisappeared.java

--- Patterns/arrays/NumbersDisappeared.java
+++ Patterns/arrays/NumbersDisappeared.java
@@ -18,3 +18,18 @@ class Solution {
 }
 
 // get o(1) space solution
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+      List<Integer> res = new ArrayList();
+      
+      for(int num : nums){
+        int index = Math.abs(num) - 1;
+        if(nums[index] > 0) nums[index] = -nums[index];          
+      }
+      
+      for(int i = 0; i < nums.length; i++){
+        if(nums[i] > 0) res.add(i + 1);      
+      }
+      return res;
+    }
+}
