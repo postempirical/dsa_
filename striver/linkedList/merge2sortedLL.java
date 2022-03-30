187daaa480d4b384683e6bad248a9938854bcc91
postempirical <53400689+postempirical@users.noreply.github.com>
2022-03-30 06:07:03 +0000

    Update merge2sortedLL.java

--- striver/linkedList/merge2sortedLL.java
+++ striver/linkedList/merge2sortedLL.java
@@ -1,26 +1,36 @@
 // https://leetcode.com/problems/merge-two-sorted-lists/
-
 class Solution {
     public ListNode mergeTwoLists(ListNode a, ListNode b) {
-        ListNode preHead = new ListNode(0);
-        ListNode mover = preHead;
+        if (a == null) return b;
+        if (b == null) return a;
+        
+        // create dummy list with 0 as dummy node
+        ListNode dummy = new ListNode(0);
+        // point curr at dummy node 0
+        ListNode curr = dummy;
         
         while (a != null && b != null) {
-            if (a.val > b.val) {
-                mover.next = b;
-                b = b.next;
+            // point next of curr of lesser val
+            // move smaller list head forward
+            // move curr forward
+            if (a.val <= b.val) {
+                curr.next = a;
+                a = a.next;
+                curr = curr.next;
             }
             else {
-                mover.next = a;
-                a = a.next;
+                curr.next = b;
+                b = b.next;
+                curr = curr.next;
             }
-            mover = mover.next;
         }
         
-        if (a == null) mover.next = b;
-        else mover.next = a;
+        // add remaining list
+        if (a == null) curr.next = b;
+        else if (b == null) curr.next = a;
         
-        return preHead.next;
+        // dummy points at 0, point to its next for list start
+        return dummy.next;
     }
 }
 
@@ -33,6 +43,8 @@ class Solution {
         ListNode dummyHead = new ListNode(0);        
         ListNode mover = dummyHead;
         
+				// bring dummy, mover on smaller node
+				// separate that node by adding null to mover next
         if (a.val < b.val) {
             dummyHead = a;
             mover = a;
@@ -46,6 +58,10 @@ class Solution {
             mover.next = null;
         }
         
+				// point mover to smaller val
+				// move mover to smaller node
+				// move smaller list node forward
+				// always separate by adding null
         while (a != null && b != null) {
             if (a.val < b.val) {
                 mover.next = a;
@@ -60,6 +76,7 @@ class Solution {
             mover.next = null;
         }
         
+				// add remaining list
         if (a == null) mover.next = b;
         else mover.next = a;
         
