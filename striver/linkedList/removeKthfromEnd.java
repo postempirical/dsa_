af87fa589508f002267c25c7532085c637d80d26
postempirical <53400689+postempirical@users.noreply.github.com>
2022-04-01 14:52:30 +0000

    Update removeKthfromEnd.java

--- striver/linkedList/removeKthfromEnd.java
+++ striver/linkedList/removeKthfromEnd.java
@@ -2,16 +2,25 @@
 
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode start = new ListNode(0, head);
-        ListNode fast = start, slow = start;
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode fast = dummy;
+        ListNode slow = dummy;
         
-        for (int i = 0; i < n; i++) fast = fast.next;
+        // bring fast to nth node
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
         
+        // make slow travel len-n dis
         while (fast.next != null) {
             fast = fast.next;
             slow = slow.next;
         }
+        
+        // delete the node ahead of alow
         slow.next = slow.next.next;
-        return start.next;
+        
+        return dummy.next;
     }
 }
