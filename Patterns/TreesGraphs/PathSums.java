89bea307155f59dfa22148f4fa9a59995e5b7ef8
postempirical <53400689+postempirical@users.noreply.github.com>
2022-04-10 10:34:42 +0000

    Update PathSums.java

--- Patterns/TreesGraphs/PathSums.java
+++ Patterns/TreesGraphs/PathSums.java
@@ -1,3 +1,4 @@
+// PATH SUM 1
 class Solution {
     public boolean hasPathSum(TreeNode root, int sum) {
         if (root == null) return false;
@@ -9,3 +10,30 @@ class Solution {
         return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
     }
 }
+
+
+// PATH SUM 2
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        List<Integer> current = new ArrayList<>();
+        findPath(root, targetSum, current, res);
+        return res;
+    }
+    private void findPath(TreeNode node, int sum, 
+                          List<Integer> current, List<List<Integer>> res) {
+        if (node == null) return;
+        current.add(node.val);
+        // if leaf node and sum matches, add current list to res list
+        if (node.left == null && node.right == null && sum == node.val)
+            res.add(new ArrayList<Integer>(current));
+        // else go to its left/right side with remainig sumt to hit
+        findPath(node.left, sum - node.val, current, res);
+        findPath(node.right, sum - node.val, current, res);
+        // backtrack and remove last value
+        current.remove(current.size() - 1);
+    }
+}
+
+
+// PATH SUM 3
