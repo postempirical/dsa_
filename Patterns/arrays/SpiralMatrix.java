26c50240a8284723a0bf3e9423ffc4ba13f37c81
postempirical <53400689+postempirical@users.noreply.github.com>
2022-04-13 03:50:57 +0000

    Update SpiralMatrix.java

--- Patterns/arrays/SpiralMatrix.java
+++ Patterns/arrays/SpiralMatrix.java
@@ -62,3 +62,39 @@ class Solution {
         return res;
     }
 }
+
+
+
+// spiral matrix 2
+// fill from 1 to n^2 in square matreix of len n
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] matrix = new int[n][n];
+        List<Integer> res = new ArrayList<>();
+        int top = 0,  bottom = n - 1;
+        int left = 0, right = n - 1;
+        int dir = 0;
+        // value to be inserted is equal to total cells in matrix
+        int value = 1;
+        while (top <= bottom && left <= right) {
+            if (dir == 0) {
+                for (int i = left; i <= right; i++) matrix[top][i] = value++;
+                top++;
+            }
+            else if (dir == 1) {
+                for (int i = top; i <= bottom; i++) matrix[i][right] = value++;
+                right--;
+            }
+            else if (dir == 2) {
+                for (int i = right; i >= left; i--) matrix[bottom][i] = value++;
+                bottom--;
+            }
+            else {
+                for (int i = bottom; i >= top; i--) matrix[i][left] = value++;
+                left++;
+            }
+            dir = (dir + 1) % 4;
+        }
+        return matrix;
+    }
+}
