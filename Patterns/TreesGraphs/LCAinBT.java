70bcec8cbe1778ee6de9f90ec899b14d2cbe08c2
postempirical <53400689+postempirical@users.noreply.github.com>
2022-04-13 07:07:24 +0000

    Create LCAinBT.java

---
+++ Patterns/TreesGraphs/LCAinBT.java
@@ -0,0 +1,12 @@
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left == null) return right;
+        if (right == null) return left;
+        // above conditions also consider if both are null
+        // if both are non null we found lca
+        return root;
+    }
+}
