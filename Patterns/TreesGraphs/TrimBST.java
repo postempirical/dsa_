680428edec03701c121c664054ed5bbcaafb3dee
postempirical <53400689+postempirical@users.noreply.github.com>
2022-04-15 02:50:04 +0000

    Create TrimBST.java

---
+++ Patterns/TreesGraphs/TrimBST.java
@@ -0,0 +1,50 @@
+class Solution {
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root == null) return null;
+        // if out of range trim that branch and switch to other one
+        if (root.val < low) return trimBST(root.right, low, high);
+        if (root.val > high) return trimBST(root.left, low, high);
+        
+        // otherwise go normally as done in a bst
+        root.left = trimBST(root.left, low, high);
+        root.right = trimBST(root.right, low, high);
+        return root;
+    }
+}
+
+// Iterative
+class Solution {
+    public TreeNode trimBST(TreeNode root, int L, int R) {
+        if (root == null) {
+            return root;
+        }
+        //Find a valid root which is used to return.
+        while (root.val < L || root.val > R) {
+            if (root.val < L) {
+                root = root.right;
+            }
+            if (root.val > R) {
+                root = root.left;
+            }
+        }
+        TreeNode dummy = root;
+        // Remove the invalid nodes from left subtree.
+        while (dummy != null) {
+            while (dummy.left != null && dummy.left.val < L) {
+                dummy.left = dummy.left.right; 
+                // If the left child is smaller than L, then we just keep the right subtree of it. 
+            }
+            dummy = dummy.left;
+        }
+        dummy = root;
+        // Remove the invalid nodes from right subtree
+        while (dummy != null) {
+            while (dummy.right != null && dummy.right.val > R) {
+                dummy.right = dummy.right.left;
+                // If the right child is biggrt than R, then we just keep the left subtree of it. 
+            }
+            dummy = dummy.right;
+        }
+        return root;
+    }
+}
