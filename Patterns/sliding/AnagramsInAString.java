52d77cbdf808b1a8e74d85a07e15b9d8a65fe11d
postempirical <53400689+postempirical@users.noreply.github.com>
2022-07-16 11:02:29 +0000

    Update AnagramsInAString.java

--- Patterns/sliding/AnagramsInAString.java
+++ Patterns/sliding/AnagramsInAString.java
@@ -40,15 +40,51 @@ class Solution {
                 
                 begin++;
             }
-            // if (end - begin == p.length()) {
-            //     if (count == 0) res.add(begin);
-            //     char temp = s.charAt(begin);
-            //     if (map.containsKey(temp)) {
-            //         map.put(temp, map.get(temp) + 1);
-            //         if (map.get(temp) > 0) count++;
-            //     }
-            //     begin++;
-            // }
+        }
+        return res;
+    }
+}
+
+
+// av approach
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        // s is a word, p is the pattern
+        List<Integer> res = new ArrayList<>();
+        
+        // // character hash
+        // using this to track if window fits pattern later on
+        HashMap<Character, Integer> map = new HashMap<>();
+        
+        // make a character, its frequency map
+        for(char c : p.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        
+        // map size will give total no of unique chars
+        int count = map.size();
+        int i = 0, j = 0;
+        
+        while (j < s.length()) {
+            // if same char, dec its freq
+            if (map.containsKey(s.charAt(j))) {
+                map.put(s.charAt(j), map.get(s.charAt(j)) - 1);
+                // decrease count if freq gets 0
+                if (map.get(s.charAt(j)) == 0) count--;   
+            }
+            
+            if (j - i + 1 == p.length()) {
+                // adding start index if anagram found
+                if (count == 0) res.add(i);
+                    
+                // now update for next window
+                if (map.containsKey(s.charAt(i))) {
+                    map.put(s.charAt(i), map.get(s.charAt(i)) + 1);
+                    if (map.get(s.charAt(i)) == 1) count++;
+                }
+                i++;
+            }
+            j++;
         }
         return res;
     }
