d9c4a1401f5099c11f2cca324fb999dd6b5f266a
postempirical <53400689+postempirical@users.noreply.github.com>
2022-07-16 13:54:37 +0000

    Update longestSubStrNorepeat.java

--- striver/slidingWindow/longestSubStrNorepeat.java
+++ striver/slidingWindow/longestSubStrNorepeat.java
@@ -1,37 +1,40 @@
-// https://leetcode.com/problems/longest-substring-without-repeating-characters/
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         HashMap<Character, Integer> map = new HashMap<>();
-        int i = 0, j = 0;
-        int mx = 0;
+        int begin = 0, end = 0;
+        int max = 0;
         
-        while (j < s.length()) {
+        while (end < s.length()) {
+            char c = s.charAt(end);
+            
             // if map has char, inc its freq else initiate as 1
-            if (map.containsKey(s.charAt(j))) 
-                map.put(s.charAt(j), map.get(s.charAt(j)) + 1);
-            else map.put(s.charAt(j), 1);
+            if (map.containsKey(c)) 
+                map.put(c, map.get(c) + 1);
+            else map.put(c, 1);      
+            
+            int count = map.size();
             
             // if size is x and window size is also x 
             // this implies they are all unique
-            if (map.size() == j - i + 1) {
-                mx = Math.max(mx, j - i + 1);
-            }
-            
-            // if size is less then window size
+            if (count == end - begin + 1) 
+                max = Math.max(max, end - begin + 1);
+         
+            // if map size is less then window size
             // it implies there are extra chars in window
-            if (map.size() < j - i +1) {
+            if (count < end - begin + 1) {
                 // keep removing from start and dec the freq
                 // also if freq gets 0, remove elem to dec size
-                while (map.size() < j - i + 1) {
-                    if (map.containsKey(s.charAt(i)))
-                        map.put(s.charAt(i), map.get(s.charAt(i)) - 1);
-                    if (map.get(s.charAt(i)) == 0)
-                        map.remove(s.charAt(i));
-                    i++;
+                while (count < end - begin + 1) {
+                    char temp = s.charAt(begin);
+                    if (map.containsKey(temp))
+                        map.put(temp, map.get(temp) - 1);
+                    if (map.get(temp) == 0)
+                        map.remove(temp);
+                    begin++;
                 }
             }
-            j++;
+            end++;
         }
-        return mx;
+        return max;
     }
 }
