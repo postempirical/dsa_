451793c11c38c5a7a4b85e71b9989708c3971d2f
postempirical <53400689+postempirical@users.noreply.github.com>
2022-07-22 11:08:19 +0000

    Update letterCasePermute.java

--- striver/recursion/letterCasePermute.java
+++ striver/recursion/letterCasePermute.java
@@ -1,4 +1,33 @@
 // https://leetcode.com/problems/letter-case-permutation/
+
+class Solution {
+    public List<String> letterCasePermutation(String s) {
+        List<String> res = new ArrayList<>();
+        backtrack(0, res, s.toCharArray());
+        return res;
+    }
+    
+    private void backtrack(int index, List<String> res, char[] str) {
+        // if we reach string end, add permutation to result list
+        if (index == str.length) res.add(new String(str));
+
+        else {
+            // if current index is a letter
+            // make a branch for upper and another for lower
+            if (Character.isLetter(str[index])) {
+                str[index] = Character.toUpperCase(str[index]);
+                backtrack(index + 1, res, str);
+                
+                str[index] = Character.toLowerCase(str[index]);
+                backtrack(index + 1, res, str);
+            }
+            // else if it not a letter, simply move to next index
+            else backtrack(index + 1, res, str);
+        }
+    }
+}
+
+// ADITYA VERMA APPROACH
 class Solution {
     public List<String> letterCasePermutation(String s) {
         String output = "";
