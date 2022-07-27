152e3e4a573b8299783e0a8655dd65b6db1a475c
postempirical <53400689+postempirical@users.noreply.github.com>
2022-07-27 14:49:48 +0000

    Update CourseSchedule.java

--- Patterns/TreesGraphs/CourseSchedule.java
+++ Patterns/TreesGraphs/CourseSchedule.java
@@ -1,3 +1,4 @@
+// COURSE SCHEDULE 1
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         // KAHNS ALGO
@@ -35,5 +36,57 @@ class Solution {
         }
         // if count != total nodes => there is cycle
         return count == numCourses;
+    }
+}
+
+// COURSE SCHEDULE 2
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        // KAHNS ALGO
+        List<Integer>[] graph = new LinkedList[numCourses];
+        Queue<Integer> q = new LinkedList<>();
+        int[] indegree = new int[numCourses];
+        int count = 0;
+        int[] topo = new int[numCourses];
+        
+        // graph will have numcourses amout lists inside it
+        for (int i = 0; i < numCourses; i++) graph[i] = new LinkedList<>();
+        
+        // graph index is pair 1, its neighbours are as list pair 0
+        // increase indegree of pair 0th value
+        for (int[] pair : prerequisites) {
+            graph[pair[1]].add(pair[0]);
+            indegree[pair[0]]++;
+        }
+        
+        // add all 0 indegree nodes to queue
+        // also add the node to topo as there are no prerequisites for it
+        int index = 0;
+        for (int i = 0; i < indegree.length; i++) {
+            if (indegree[i] == 0) {
+                q.add(i);
+                topo[index++] = i;
+            }
+        }
+        
+        // increase count when poll from queue
+        // visit all the neighbours of current index course
+        // decrease indegree of neighbours
+        // add to q whenever indegree get 0
+        while (!q.isEmpty()) {
+            int course = q.poll();
+            count++;
+            for (int neighbour : graph[course]) {
+                indegree[neighbour]--;
+                if(indegree[neighbour] == 0) {
+                    topo[index++] = neighbour;
+                    q.add(neighbour);
+                }
+            }
+        }
+        
+        // if count != total nodes => there is cycle
+        if (count == numCourses) return topo;
+        else return new int[0];
     }
 }
