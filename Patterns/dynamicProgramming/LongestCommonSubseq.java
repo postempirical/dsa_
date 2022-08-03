9e86f1dd15c04695d5173571e55197a228776f0a
postempirical <53400689+postempirical@users.noreply.github.com>
2022-08-03 20:50:50 +0000

    Update LongestCommonSubseq.java

--- Patterns/dynamicProgramming/LongestCommonSubseq.java
+++ Patterns/dynamicProgramming/LongestCommonSubseq.java
@@ -22,3 +22,26 @@ class Solution {
 }
 
 // TABULAR
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int n1 = text1.length(), n2 = text2.length();
+        int dp[][]=new int[n1 + 1][n2 + 1];
+        
+        // right shifted indices so ind now becomes ind - 1
+        
+        // base case
+        for (int ind1 = 0; ind1 <= n1; ind1++) dp[ind1][0] = 0;
+        for (int ind2 = 0; ind2 <= n2; ind2++) dp[0][ind2] = 0;
+        
+        // loops in reverse
+        for (int ind1 = 1; ind1 <= n1; ind1++) {
+            for (int ind2 = 1; ind2 <= n2; ind2++) {
+                // copy recurrence
+                if (text1.charAt(ind1 - 1) == text2.charAt(ind2 - 1)) // shift
+                    dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1];
+                else dp[ind1][ind2] = Math.max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]);
+            }
+        }
+        return dp[n1][n2];
+    }
+}
