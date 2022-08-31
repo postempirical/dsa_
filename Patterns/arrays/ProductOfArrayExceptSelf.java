bbf0525852b6653529d5d216bf95daf0d0a290f7
postempirical <53400689+postempirical@users.noreply.github.com>
2022-08-31 08:45:05 +0000

    Update ProductOfArrayExceptSelf.java

--- Patterns/arrays/ProductOfArrayExceptSelf.java
+++ Patterns/arrays/ProductOfArrayExceptSelf.java
@@ -1,21 +1,26 @@
 
-o(n) 0(n)
+// o(n) 0(n)
 class Solution {
     public int[] productExceptSelf(int[] nums) {
+        int[] product = new int[nums.length];
         int[] left = new int[nums.length];
         int[] right = new int[nums.length];
         
+        // 01-02-03-04 = nums
+        // 01-01-02-06 = left
+        // 24-12-04-01 = right
+        // 24-12-08-06 = result
+        
         left[0] = 1;
         for (int i = 1; i < nums.length; i++) {
-            left[i] = left[i-1] * nums[i-1];
+            left[i] = left[i - 1] * nums[i - 1];
         }
         
         right[nums.length - 1] = 1;
         for (int i = nums.length - 2; i >= 0; i--) {
-            right[i] = right[i+1] * nums[i+1];
+            right[i] = right[i + 1] * nums[i + 1];
         }
         
-        int[] product = new int[nums.length];
         for (int i = 0; i < product.length; i++) {
             product[i] = left[i] * right[i];
         }
@@ -26,22 +31,27 @@ class Solution {
 
 // o(n) o(1)
 class Solution {
-    public int[] productExceptSelf(int[] nums) {
+    public int[] productExceptSelf(int[] nums) {        
+        // 01-02-03-04 = nums
+        // 01-01-02-06 = res(left)
+        // 24-12-04-01 = prod
+        // 24-12-08-06 = result(final)
+        
         int[] res = new int[nums.length];
-        int pre_left = 1;
         
-        for (int i = 0; i < nums.length; i++) {
-            pre_left *= nums[i];
-            res[i] = pre_left;
+        // store left products in res array initially
+        res[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
         }
         
-        int post_right = 1;
-        for (int i = nums.length - 1; i > 0; i--) {
-            res[i] = res[i - 1] * post_right;
-            post_right *= nums[i];
+        // product of numbers to the right of current element.
+        int prod = 1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            prod = prod * nums[i + 1];
+            res[i] *= prod;
         }
         
-        res[0] = post_right;
         return res;
     }
 }
