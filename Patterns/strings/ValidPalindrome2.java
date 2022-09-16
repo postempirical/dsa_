2a4a7479b4b8aef8824f55179812fc7d2a3bf5f7
postempirical <53400689+postempirical@users.noreply.github.com>
2022-09-16 20:24:43 +0000

    Update ValidPalindrome2.java

--- Patterns/strings/ValidPalindrome2.java
+++ Patterns/strings/ValidPalindrome2.java
@@ -1,22 +1,24 @@
 class Solution {
     public boolean validPalindrome(String s) {
-        int i = 0;
-        int j = s.length() - 1;
-        while (i < j) {
-            // if values are inequal
-            if (s.charAt(i) != s.charAt(j)) {
-                // check if other ranges ahead are palindrome or not
-                // recursive keep going on both ends
-                return isPalindrome(s, i + 1, j) || isPalindrome(s, i, j - 1);
+        // if not equal we can either skip start or end
+        // and then check if after skipping, any new range is palindrome
+        // m x a d a m
+        // x a d a
+        // x a d || [a d a] 
+        // a d a returns true
+        int start = 0, end = s.length() - 1;
+        while (start < end) {          
+            if (s.charAt(start) != s.charAt(end)) {
+                return isPalindrome(start + 1, end, s) || isPalindrome(start, end - 1, s);
             }
-            // if chars are equals simply move to next values
-            i++;
-            j--;
+            // simply move ahead if equal
+            start++;
+            end--;
         }
         return true;
     }
     
-    private boolean isPalindrome(String s, int i, int j) {
+    private boolean isPalindrome(int i, int j, String s) {
         while (i < j) {
             if (s.charAt(i) != s.charAt(j)) return false;
             i++;
