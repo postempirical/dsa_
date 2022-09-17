6f7f64a03fbf32d22415d5e4225e988b2f6ff42d
postempirical <53400689+postempirical@users.noreply.github.com>
2022-09-17 19:17:56 +0000

    Update longestPanSub.java

--- striver/strings/longestPanSub.java
+++ striver/strings/longestPanSub.java
@@ -1,33 +1,34 @@
 // https://leetcode.com/problems/longest-palindromic-substring/
 class Solution {
     public String longestPalindrome(String s) {
-        int start = 0, end = 0;
+        // from every index we expand to both left and right simulataneously
+        // we keep going till chars are equal
+        // as soon as an expansion ends, we check for max
+        // for odd palins -> b a [b] a d
+        // for even palin -> a b [b] [b] b d
+        String maxstring = "";
+        int start, end;
         for (int i = 0; i < s.length(); i++) {
-						// place both at the cureent char
-            int left = i, right = i;
-            char c = s.charAt(i);
+            // odd length palindromes
+            start = i; end = i;
+            String s1 = expand(start, end, s);
             
-						// updation for even char 
-            while (left >= 0 && s.charAt(left) == c) left--;
-            while (right < s.length() && s.charAt(right) == c) right++;
+            // even length palindromes
+            start = i; end = i + 1;
+            String s2 = expand(start, end, s);
             
-						// check palindrome in range of current l,r
-            while (left >= 0 && right < s.length()) {
-                if (s.charAt(left) != s.charAt(right)) break;
-                left--;
-                right++;
-            }
-					
-						// get them in actual range
-            left = left + 1;
-            right = right - 1;
-            
-						// update max
-            if (end - start < right - left) {
-                start = left;
-                end = right;
-            }
+            // if lengths receinved are greater than max, update result
+            if (s1.length() > maxstring.length()) maxstring = s1;
+            if (s2.length() > maxstring.length()) maxstring = s2;
         }
-        return s.substring(start, end + 1);
+        return maxstring;
+    }
+    
+    private String expand(int start, int end, String str) {
+        while (start >= 0 && end < str.length()) {
+            if (str.charAt(start) != str.charAt(end)) break;
+            start--; end++;
+        }
+        return str.substring(start + 1, end);
     }
 }
